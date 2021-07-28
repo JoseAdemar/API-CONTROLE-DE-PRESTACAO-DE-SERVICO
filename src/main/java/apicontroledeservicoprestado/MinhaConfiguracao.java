6dd60730a9d397cfa076c4ff43309b2c9bae4a3c
Jose Ademar De Oliveira Junior <jose.oliveira@palmares.gov.br>
2021-07-28 13:39:40 -0300

    Primeiro commit criação do projeto

---
+++ src/main/java/apicontroledeservicoprestado/MinhaConfiguracao.java
@@ -0,0 +1,14 @@
+package apicontroledeservicoprestado;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguracao {
+
+	 @Bean(name = "applicationName")
+	 public String applicationName() {
+		 
+		 return "Sistema de vendas";
+	 }
+}
---
+++ src/test/java/apicontroledeservicoprestado/ApiControleDeServicoPrestadoApplicationTests.java
@@ -0,0 +1,13 @@
+package apicontroledeservicoprestado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControleDeServicoPrestadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
