6c62dbe21f1a10188729ec9ebe726a86c83ef31a
jose_ademar <joseademar.junior@shift.com.br>
2021-09-21 08:54:39 -0300

    Criado o method de buscar cliente por ID

--- src/main/java/apicontroledeservicoprestado/controller/ClienteController.java
+++ src/main/java/apicontroledeservicoprestado/controller/ClienteController.java
@@ -3,6 +3,8 @@ package apicontroledeservicoprestado.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,15 @@ public class ClienteController {
 	@PostMapping
 	public ResponseEntity<Cliente> persisteCliente(@RequestBody Cliente cliente) {
 
-		Cliente persiste = clienteService.salvar(cliente);
+		Cliente persiste = clienteService.persisteUmCliente(cliente);
 		return ResponseEntity.status(HttpStatus.CREATED).body(persiste);
 
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscaClientePorId(@PathVariable Long id) {
+
+		Cliente buscarClientePorId = clienteService.buscaUmClientePorId(id);
+		return ResponseEntity.status(HttpStatus.OK).body(buscarClientePorId);
 	}
 }
--- src/main/java/apicontroledeservicoprestado/service/ClienteService.java
+++ src/main/java/apicontroledeservicoprestado/service/ClienteService.java
@@ -1,7 +1,9 @@
 package apicontroledeservicoprestado.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import apicontroledeservicoprestado.entity.Cliente;
 import apicontroledeservicoprestado.repository.ClienteRepository;
@@ -16,10 +18,17 @@ public class ClienteService {
 		this.clienteRepository = clienteRepository;
 	}
 
-	public Cliente salvar(Cliente cliente) {
+	public Cliente persisteUmCliente(Cliente cliente) {
 
 		return clienteRepository.save(cliente);
 	}
-	
-	
+
+	public Cliente buscaUmClientePorId(Long id) {
+
+		return clienteRepository.findById(id)
+			   .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		
+		
+	}
+
 }
